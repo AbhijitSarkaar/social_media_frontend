60f46a4d109594813a892e3504728645d0725bee
Abhijit Sarkar <abhijit.sarkar@embibe.com>
2020-11-10 11:17:46 +0530

    Add email and password fields

--- src/App.js
+++ src/App.js
@@ -1,42 +1,39 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 import './App.scss';
-import Users from './Users';
-import SideBar from './SideBar';
+import Users from './components/Users';
+import SideBar from './components/SideBar';
+import LogIn from './components/LogIn';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      users: []
-    }
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			users: [],
+		};
+	}
 
-  componentDidMount() {
-    axios('http://localhost:5000/users')
-    .then(res => {
-      this.setState({
-        users: res.data
-      })
-    })
-  }
-  render() {
-    return (
-      <div className="App">
-        <div className="header">
-          <p className="header-text">
-            Social Media
-          </p>
-        </div>
-        <div className="body-container">
-          <SideBar />
-          <Users 
-            users={this.state.users}
-          />
-        </div>
-      </div>
-    ); 
-  }
+	componentDidMount() {
+		axios('http://localhost:5000/users').then((res) => {
+			this.setState({
+				users: res.data,
+			});
+		});
+	}
+	render() {
+		return (
+			<div className='App'>
+				<div className='header'>
+					<p className='header-text'>Social Media</p>
+				</div>
+				<div className='body-container'>
+					{/* <SideBar /> */}
+					{/* <Users users={this.state.users} /> */}
+				</div>
+				<LogIn />
+			</div>
+		);
+	}
 }
 
 export default App;
