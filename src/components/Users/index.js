d9f35e060576dc6d9263ca3f143a6810a3922ded
Abhijit Sarkar <abhijit.sarkar@embibe.com>
2020-11-19 15:49:32 +0530

    Put users api call in Users component

--- src/components/Users/index.js
+++ src/components/Users/index.js
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import User from './User';
+import axios from 'axios';
 import './index.scss';
 
-const Users = (props) => {
-    return (
-        <div className="users-container">
-            <div className="users-table-header">
-                <div>Id</div>
-                <div>First Name</div>
-                <div>Last name</div>
-            </div>
-            {
-            props.users.map(user => 
-                <User 
-                    user={user}
-                />
-            )}   
-        </div>
-    );
-};
-
-export default Users;
+export default class Users extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			users: [],
+		};
+	}
+	componentDidMount() {
+		axios('http://localhost:5000/users').then((res) => {
+			this.setState({
+				users: res.data,
+			});
+		});
+	}
+	render() {
+		return (
+			<div className='users-container'>
+				<div className='users-table-header'>
+					<div>Id</div>
+					<div>First Name</div>
+					<div>Last name</div>
+				</div>
+				{this.state.users.map((user) => (
+					<User user={user} />
+				))}
+			</div>
+		);
+	}
+}
