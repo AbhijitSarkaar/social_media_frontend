b3ba939d904dbf7cf842e9cabb18e21bb14892b3
Abhijit Sarkar <abhijit.sarkar@embibe.com>
2020-11-19 15:48:43 +0530

    Add router

--- src/App.js
+++ src/App.js
@@ -1,40 +1,25 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import './App.scss';
+import { Switch, Route } from 'react-router-dom';
 import Users from './components/Users';
-import SideBar from './components/SideBar';
-import LogIn from './components/LogIn';
-import SignUp from './components/SignUp';
 import UserProfile from './components/UserProfile';
-
+import SignUp from './components/SignUp';
+import LogIn from './components/LogIn';
+import Error from './components/Error';
+import './App.scss';
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			users: [],
-		};
-	}
-
-	componentDidMount() {
-		axios('http://localhost:5000/users').then((res) => {
-			this.setState({
-				users: res.data,
-			});
-		});
-	}
 	render() {
 		return (
 			<div className='App'>
 				<div className='header'>
 					<p className='header-text'>Social Media</p>
 				</div>
-				<div className='body-container'>
-					{/* <SideBar /> */}
-					{/* <Users users={this.state.users} /> */}
-				</div>
-				{/* <LogIn /> */}
-				{/* <SignUp /> */}
-				<UserProfile />
+				<Switch>
+					<Route path='/signup' component={SignUp} />
+					<Route path='/login' component={LogIn} />
+					<Route path='/user/:id' component={UserProfile} />
+					<Route path='/users' component={Users} />
+					<Route component={Error} />
+				</Switch>
 			</div>
 		);
 	}
